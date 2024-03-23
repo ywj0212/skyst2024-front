db691ddd1ef39c62350ad579bb03ee5cd1eb4efb
유우진 <ywj02120212@gmail.com>
2024-03-23 15:53:57 +0900

    Feat: basic layout for Dashboard, Landing, Login Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function Landing() {
   return <>
-
+    <div>
+      <h1>타임 캡슐</h1>
+      <h2>프로젝트에 대한 한 줄 설명</h2>
+      <button>시작하기</button>
+      <FontAwesomeIcon icon="fa-solid fa-chevron-down" />
+    </div>
+    <div>
+      <p>이 서비스에 대한 간략한 설명</p>
+    </div>
+    <footer>
+      <p>우리 팀과 대회 소개</p>
+    </footer>
   </>;
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,28 @@
 function Login() {
   return <>
+      <div className="flex min-h-screen content-center">
+      <aside className="flex-1">
+        <img className="absolute w-64 top-1/2 transform -translate-y-1/2 left-8" src="http://placehold.it/1080x2000?text=phone_mockup" />
+        <img className="absolute w-72 top-1/2 transform -translate-y-1/2 left-36 shadow-2xl shadow-slate-700/20" src="http://placehold.it/1080x2000?text=phone_mockup" />
+      </aside>
+      <div className="flex-1">
+        <div className="">
+          <img className="" src="http://placehold.it/300x100?text=logo_text" />
 
+          <label htmlFor="email">이메일</label>
+          <input type="email" name="email" />
+          <label htmlFor="password">비밀번호</label>
+          <input type="password" name="password" />
+          <input type="submit" value="" />
+          <div></div>
+          <button>카카오톡으로 로그인</button>
+          <button>Google로 로그인</button>
+        </div>
+        {/* <div className="flex-1 bg-indigo-300">
+          
+        </div> */}
+      </div>
+    </div>
   </>;
 }
 
