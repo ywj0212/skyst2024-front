51ce5a5eeeb6d05b73e2b4b62d0dd93c6e25c536
유우진 <ywj02120212@gmail.com>
2024-03-24 05:31:51 +0900

    Feat: Noti

--- src/components/NotificationCard.jsx
+++ src/components/NotificationCard.jsx
@@ -1,6 +1,6 @@
 const NotificationCard = ({ question }) => {
   return (
-    <div className="group max-w-2xl mt-10 overflow-hidden rounded-2xl shadow-xl shadow-indigo-100 dark:bg-gray-800">
+    <div className="opacity-100 group max-w-2xl m-0 mx-auto mt-16 overflow-hidden rounded-2xl shadow-xl shadow-black/10">
       <div className="bg-gradient-to-tr from-red-200 to-amber-300 rounded-2xl relative">
         <h2 className="text-sm text-red-500 pt-8 px-4">오늘 열린 영상 보러가기</h2>
         <h1 className="text-xl font-bold text-white pb-8 px-4">
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,8 +13,14 @@ function Dashboard() {
 
   useEffect(() => {
     getVideos();
+    // setIsNotification(true)
+    setTimeout(() => setIsNotification(true), 1000)
   }, [])
 
+  const [isNotification, setIsNotification] = useState(false)
+  const Notification = () => {
+    
+  }
   const getVideos = async () => {
     const response = await fetch("https://api-skyst.mirix.kr/video/all/skyst2024/", {
       method: "GET", // 또는 "POST" 만약 백엔드가 POST 요청을 처리한다면
@@ -31,10 +37,15 @@ function Dashboard() {
 
   return (
     <div className="bg-slate-100 pb-24">
+      <div className="hidden h-80 opacity-80"></div>
+      <div className="hidden h-0 opacity-0"></div>
+      <div className={"transition duration-300 flex-col " + (isNotification ? " h-80 opacity-100 " : " h-0 opacity-0 ") + " z-10 absolute inset-x-0 top-0 bg-gradient-to-b from-amber-400/70 to-transparent"}>
+        <div className="flex-1 mx-auto">
+          <NotificationCard question={"지금 당장 자식에게 하고 싶은 말은 무엇인가요?"} />
+        </div>
+      </div>
       <Container>
-        <Navbar name={"다해"} />
-        <NotificationCard question={"지금 당장 자식에게 하고 싶은 말은 무엇인가요?"} />
-
+        <Navbar onClick={Notification} name={"다해"} />
         <div className="mt-16 flex justify-between items-center">
           <div className="min-w-0 flex-1">
             <h2 className="text-2xl leading-7 text-slate-800 sm:truncate sm:text-3xl sm:tracking-tight">
@@ -62,15 +73,22 @@ function Dashboard() {
                       thumbnailUrl={"https://cdn.pixabay.com/photo/2015/09/05/21/51/reading-925589_1280.jpg"}
                       availableAfter={"2024년 4월 1일"}
                     />
-        <ProductCard
-                      date={"2024년 3월 23일"}
-                      today={false}
-                      question={"오늘 당신의 기분은 어떠신가요?"}
-                      profileUrl={"https://cdn.pixabay.com/photo/2021/02/07/02/35/woman-5989926_1280.jpg"}
-                      name={"김영희"}
-                      thumbnailUrl={"https://cdn.pixabay.com/photo/2015/09/05/21/51/reading-925589_1280.jpg"}
-                      availableAfter={"2024년 4월 1일"}
-                    />
+                    
+        {videos.map((v, idx) => (
+          <Link to="/player" state={{ url: v.url }}>
+            <ProductCard
+              key={idx}
+              date={v.datetime}
+              today={false}
+              question={v.question}
+              profileUrl={
+                "https://cdn.pixabay.com/photo/2021/02/07/02/35/woman-5989926_1280.jpg"
+              }
+              name={"김영희"}
+              thumbnailUrl={v.thumbnail}
+            />
+          </Link>
+        ))}
       </Container>
       <BottomNavigation addButton={false} />
     </div>
